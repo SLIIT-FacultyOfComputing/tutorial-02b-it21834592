2b5cac2e75bccdf096f267c6e993aee5b8ed12d9
IT21834592 <110513673+it21834592@users.noreply.github.com>
2022-08-11 16:47:16 +0000

    nothing

--- tute04.cpp
+++ tute04.cpp
@@ -12,18 +12,48 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+    int n, r;
+    long factorial = 1.0;
+
+    cout << "Enter a value for n : ";
+    cin >> n;
+    
+    cout << "Enter a value for r : ";
+    cin >> r;
+    
+    cout << "nCr = ";
+	cout << nCr(n, r);
+	
+	cout << endl;
+    
+//    cout << "Factorial of n is : ";
+//    cout << Factorial(n);
+
+    return 0;
+}
+
+long Factorial(int no)
+{
+	long factorial = 1.0;
+	 if (no < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= no; ++i) {
+            factorial *= i;
+        }
+        return factorial;  
+    }
+}
+long nCr(int n, int r)
+{
+	int ncr;
+	ncr = Factorial(n) / (Factorial(r) * Factorial(n-r));
+	return ncr;
+	
 }
